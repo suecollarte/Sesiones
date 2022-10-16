8c870b4d38d3f1a946b4a35cd18d62b779a5d801
suecollarte <suecollarte@gmail.com>
2022-10-15 21:38:28 -0300

    Add files via upload

--- models/usuarios.model.js
+++ models/usuarios.model.js
@@ -1,1 +1,12 @@
-
+import {Schema, model} from "mongoose";
+const userSchema = new Schema({
+    user: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+export const usuarioModel = model('usuario', userSchema);
